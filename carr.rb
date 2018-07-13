f13ee59d49a7377fed7d6cbf7232103e75255009
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-13 15:34:47 +0000

    Done.

---
+++ carr.rb
@@ -0,0 +1,54 @@
+class Car 
+  
+  def initialize(model, make, year)
+    @model = model 
+    @make = make 
+    @year = year 
+  end 
+  
+  def model 
+    @model 
+  end 
+  
+  def make 
+    @make 
+  end
+  
+  def year 
+    @year
+  end 
+  
+  def heating=(heating)
+    @heating=heating 
+  end
+  
+  def heating
+    @heating 
+  end 
+  
+end 
+
+toyota = Car.new("camry","Toyota", "1998")
+toyota.model 
+toyota.make 
+toyota.year
+
+
+puts "This is my #{toyota.model}. It is a #{toyota.make} and was built in #{toyota.year}."
+
+
+new_toyota = Car.new("camry", "Toyota", "2014")
+new_toyota.model 
+new_toyota.make
+new_toyota.year
+new_toyota.heating = "heating pads"
+
+puts "This is my #{new_toyota.model}. It is a #{new_toyota.make} and was built in #{new_toyota.year}. It even has #{new_toyota.heating}."
+
+
+honda = Car.new("civic", "Honda", "2005")
+honda.model
+honda.make
+honda.year
+
+puts "This is my #{honda.model}. It is a #{honda.make} and was built in #{honda.year}."
--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,45 @@
-# book.rb
+ # book.rb
 # Add you Book class here
+
+
+class Book 
+  
+  def initialize(title)
+    @title = title 
+    
+  end 
+
+   def title 
+     @title 
+   end 
+   
+   def author=(author)
+     @author = author 
+   end 
+  
+   def author 
+     @author 
+   end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count 
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
+  
+end 
+
+summer_reading = Book.new("And Then There Were None")
+summer_reading.title 
+summer_reading.author = "Agatha Christie"
+summer_reading.genre = "Mystery"
