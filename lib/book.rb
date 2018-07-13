fa3d8259bcd82a895c4561007ffbfae8d439cfd6
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-13 18:40:43 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,43 +3,25 @@
 
 
 class Book 
+  attr_reader :title
+  attr_accessor :author, :page_count, :page_count, : genre,
   
   def initialize(title)
     @title = title 
-    
   end 
 
-   def title 
-     @title 
-   end 
-   
-   def author=(author)
-     @author = author 
-   end 
-  
-   def author 
-     @author 
-   end 
-  
-  def page_count=(num)
-    @page_count = num 
-  end 
-  
-  def page_count 
-    @page_count
-  end 
-  
-  def genre=(genre)
-    @genre = genre
-  end 
-  
-  def genre
-    @genre
-  end 
-  
+
+  def turn_page
+  puts "flipping the page...wow, you read fast!"
+end 
+
 end 
 
 summer_reading = Book.new("And Then There Were None")
 summer_reading.title 
 summer_reading.author = "Agatha Christie"
 summer_reading.genre = "Mystery"
+summer_reading.page_count = "209"
+summer_reading.turn_page 
+
+puts "The title of this book is #{summer_reading.title}. It is written by #{summer_reading.author}. The genre is #{summer_reading.genre}. There are #{summer_reading.page_count} pages. #{summer_reading.turn_page}."
